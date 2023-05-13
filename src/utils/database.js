c0c8d7c0a5cc1f00497688d871dbbe81ff70ddb3
danielPomo <danielnicolaspomo@gmail.com>
2023-05-13 01:41:27 -0300

    correcting mistakes

--- src/utils/database.js
+++ src/utils/database.js
@@ -2,7 +2,7 @@ const {Sequelize} = require("sequelize")
 require("dotenv").config()
 const db = new Sequelize({
     host: process.env.DB_HOST,
-    database: Process.env.DB_NAME,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
